43ff4a5ac08a4d5d8a70314cb32370007e6f59b9
davyvandelaar <djdavy@gmail.com>
2018-12-03 16:54:46 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hoi TOET !
+            Testen met Feature !
           </p>
           <a
             className="App-link"
